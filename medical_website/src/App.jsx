8be7f9612983e6bc3209841fe169c9f5c274ee7d
Phuctam03 <hophuctam7112003@gmail.com>
2025-03-09 03:00:39 +0700

    update tailwind

---
+++ medical_website/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div class="flex items-center justify-center h-screen">
+        <p class="text-red-500 text-2xl font-bold">Chữ màu đỏ</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
