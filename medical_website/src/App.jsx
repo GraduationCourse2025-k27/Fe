af90762cef196dcc375eb6e3e01bf0307842a2fd
Dung-048 <minhdung04082003@gmail.com>
2025-03-09 11:12:35 +0700

    this is branch DungLM

--- medical_website/src/App.jsx
+++ medical_website/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div class="flex items-center justify-center h-screen">
-        <p class="text-red-500 text-2xl font-bold">Chữ màu đỏ</p>
+        <p class="text-blue-500 text-2xl font-bold">Chữ màu đỏ</p>
       </div>
     </>
   );
