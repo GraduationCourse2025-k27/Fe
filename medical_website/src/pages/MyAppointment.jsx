f51e977272fab8a52f252d78daaba7ee4a926343
Dung-048 <minhdung04082003@gmail.com>
2025-04-17 13:43:49 +0700

    update myappointment

--- medical_website/src/pages/MyAppointment.jsx
+++ medical_website/src/pages/MyAppointment.jsx
@@ -9,34 +9,36 @@ const MyAppointment = () => {
   return (
     <div className='mt-5 pt-5'>
       <Header />
-      <div className='grid grid-cols-1 md:grid-cols-[1fr_5fr] gap-4 mx-5 mb-4'>
+      <div className='grid grid-cols-1 md:grid-cols-[1fr_5fr]  mx-5 mb-4 border rounded'>
         {/* Cột bên trái */}
-        <div className='border rounded p-4 flex flex-col items-center'>
-          <p className='pb-3 font-medium text-zinc-700 border-b w-full '></p>
-          <ul className='mt-3 space-y-2 w-full mr-5'>
-            <li className='text-md text-zinc-600 cursor-pointer border rounded  p-2 hover:bg-gray-100 text-center'>Danh sách chờ</li>
-            <li className='text-md text-zinc-600 cursor-pointer border rounded  p-2 hover:bg-gray-100 text-center'>Phê duyệt</li>
+        <div className='border-r  border-gray-300 flex flex-col items-center'>
+          <p className='p-2 font-medium text-zinc-700 border-b border-gray-300 text-center w-full '>Trạng thái lịch hẹn</p>
+          <ul className='mt-2 space-y-2 w-full mr-7'>
+            <li className='text-md text-black-600 cursor-pointer border rounded  p-2 hover:bg-gray-100 text-center'>Đang chờ xác nhận</li>
+            <li className='text-md text-black-600 cursor-pointer border rounded  p-2 hover:bg-gray-100 text-center'>Lịch xác nhận</li>
           </ul>
         </div>
 
         {/* Cột bên phải */}
-        <div>
-          <p className='pb-3 font-medium text-zinc-700 border-b'>Lịch đặt khám của tôi</p>
-          <div>
+        <div className=''>
+          
+          <p className='p-2 font-medium text-zinc-900 border-b border-gray-300 '>Lịch đặt khám của tôi</p>
+          <div className='m-2'>
             {doctors.slice(0, 2).map((item, index) => (
-              <div className='grid grid-cols-1 sm:grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
-                <div>
-                  <img className='w-full sm:w-35 bg-indigo-50' src={item.image} alt="" />
+              <div className='grid grid-cols-1 sm:grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b border-gray-300' key={index}>
+                <div className='sm:pl-5'>
+                  <img className='w-full rounded sm:w-35 bg-indigo-50' src={item.image} alt="" />
                 </div>
                 <div className='flex-1 text-sm text-zinc-600'>
                   <p className='text-neutral-800 font-semibold'>{item.name}</p>
-                  <p>{item.speciality}</p>
-                  <p className='text-zinc-700 font-medium mt-1'>{item.address}</p>
-                  <p className='text-xs mt-1'>Data time</p>
+                  <p className='text-neutral-800 font-semibold'>{item.speciality}</p>
+                  <p className='text-neutral-800 font-semibold'>{item.address}</p>
+                  <p className='text-neutral-800 font-semibold'>Data time</p>
+                  <p className='text-neutral-800 font-semibold '>Giá khám: {item.fees}</p>
                 </div>
                 <div className='flex flex-col gap-2 justify-end'>
-                  <button className='text-sm text-stone-500 text-center py-2 px-4 rounded-lg border border-gray-300 hover:bg-blue-900'>Vn Pay</button>
-                  <button className='text-sm text-stone-500 text-center py-2 px-4 rounded-lg border border-gray-300'>Hủy lịch khám</button>
+                  <button className='text-sm text-black-500 text-center py-2 px-4 rounded-lg border rounded border-gray-300 hover:bg-green-600'>Vn Pay</button>
+                  <button className='text-sm text-black-500 text-center py-2 px-4 rounded-lg border rounded border-gray-300 hover:bg-red-600'>Hủy lịch khám</button>
                 </div>
               </div>
             ))}
