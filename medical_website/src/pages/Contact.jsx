6f1149a670b61fbdd41b2f0ea41e69c83091df85
Lê Minh Dũng <minhdung04082003@gmail.com>
2025-04-21 10:09:24 +0700

    fix position icon ở form login, fix khi login hiện user, fix không nhảy trang doctor (#18)

--- medical_website/src/pages/Contact.jsx
+++ medical_website/src/pages/Contact.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import ConfirmAppointment from '../components/ConfirmAppointment'
-import NoDoctorFound from '../components/NoDoctorFound'
-import SetSuccess from '../components/SetSuccess'
+
 
 const Contact = () => {
   return (
     <div className='mt-5 pt-5'>
-      <ConfirmAppointment/>
-      <SetSuccess/>
-      <NoDoctorFound/>
+      
 
 
     
--- medical_website/vite.config.js
+++ medical_website/vite.config.js
@@ -2,10 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  clearScreen: false,
+  server: {
+    watch: {
+      ignored: [
+        '**/README.md',
+        '**/eslint.config.js',
+        '**/.git/**',
+        '**/node_modules/**',
+      ],
+    },
+  },
   build: {
     assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
-  }
+  },
 });
