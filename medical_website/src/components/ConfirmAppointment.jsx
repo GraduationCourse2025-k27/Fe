c58a8941f7095e1b05cd264ed348264a5943e669
Lê Minh Dũng <minhdung04082003@gmail.com>
2025-04-28 15:57:17 +0700

    update File ConfrimAppointment (#28)
    Co-authored-by: Phuctam03 <hophuctam7112003@gmail.com>

--- medical_website/src/components/ConfirmAppointment.jsx
+++ medical_website/src/components/ConfirmAppointment.jsx
@@ -36,16 +36,15 @@ const ConfirmAppointment = () => {
     }
   };
 
-  // if (!appointmentId) {
-  //   return null;
-  // }
+  if (!appointmentId) {
+    return null;
+  }
 
   return (
     <div className="p-4 mt-5 pt-5 sm:p-8 max-w-4xl mx-auto">
-
-{!showMessage && (
-  <h4 className="text-xl sm:text-2xl font-bold mb-6">Lịch hẹn đã đặt</h4>
-)}
+      {!showMessage && (
+        <h4 className="text-xl sm:text-2xl font-bold mb-6">Lịch hẹn đã đặt</h4>
+      )}
       {togglePayment && (
         <div className="bg-white border rounded-2xl p-4 sm:p-6 shadow-md flex flex-col sm:flex-row sm:items-center sm:justify-between">
           {/* Icon + Khám */}
